db7abc33f5fd79771e3c7d763607dd32b7346ad1
Marnix den Baars <m-denbaars1@hotmail.com>
2015-09-21 10:56:35 +0200

    Deel serverapplication en Inloggegevens

---
+++ WindowsFormsApplication1/Serverapplication.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net.Sockets;
+using System.Net;
+using System.IO;
+using System.Threading;
+
+namespace WindowsFormsApplication1
+{
+    class Serverapplication
+    {
+
+        public Serverapplication()
+        {
+            IPAddress localhost;
+
+            bool ipIsOk = IPAddress.TryParse("127.0.0.1", out localhost);
+            if (!ipIsOk) { Console.WriteLine("ip adres kan niet geparsed worden."); Environment.Exit(1); }
+
+            TcpListener listener = new System.Net.Sockets.TcpListener(localhost, 8600);
+            listener.Start();
+
+            while(true)
+            {
+                Console.WriteLine("Waiting for Client Connections");
+                TcpClient client = listener.AcceptTcpClient();
+                Thread thread = new Thread(HandleClientThread);
+                thread.Start(client);
+            }
+        }
+
+        static void HandleClientThread(object obj)
+        {
+            TcpClient client = obj as TcpClient;
+
+            bool done = false;
+            while (!done)
+            {
+                StreamReader stream = new StreamReader(client.GetStream(), Encoding.ASCII);
+                string line = stream.ReadLine();
+                Console.WriteLine("Received: {0}", line);
+                var streamwriter = new StreamWriter(client.GetStream(), Encoding.ASCII);
+                done = line.Equals("bye");
+                if (done)
+                {
+                    streamwriter.WriteLine("Bye");
+                    streamwriter.Flush();
+                }
+                else
+                {
+                    streamwriter.WriteLine("ok");
+                    streamwriter.Flush();
+                }
+
+            }
+            client.Close();
+            Console.WriteLine("Connection closed");
+
+        }
+
+    }
+}
---
+++ WindowsFormsApplication1/UserClient.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApplication1
+{
+    class UserClient
+    {
+        public string UserName;
+        public string UserAccessLevel;
+        public int UserPassword;
+
+        public UserClient(string fullNam, string aLevel, int pass)
+        {
+            fullName = fullNam;
+            accLevel = aLevel;
+            userPass = pass;
+        }
+        public string fullName
+        {
+            get { return UserName; }
+            set { UserName = value; }
+        }
+        public string accLevel
+        {
+            get { return UserAccessLevel; }
+            set { UserAccessLevel = value;  }
+        }
+        public int userPass
+        {
+            get { return UserPassword; }
+            set { UserPassword = value; }
+        }
+        public override bool Equals(object obj)
+        {
+            UserClient temp = obj as UserClient;
+            if(temp != null)
+            {
+                return (userPass == temp.userPass);
+            }
+            return false;
+        }
+
+    }
+}
