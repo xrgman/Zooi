63d8fc5a804dde4d5c3802cf78bcebff0d7eea51
ren_keijzer <jcekeijze@avans.nl>
2015-09-25 11:40:23 +0200

    measurement, session and user added

--- server application/User.cs
+++ server application/User.cs
@@ -8,5 +8,43 @@ namespace server_application
 {
     class User
     {
+        private List<Session> Sessions = new List<Session>();
+        public string userName
+        { get; private set; }
+        private string password;
+
+        public User(string username, string password)
+        {
+            this.userName = userName;
+            this.password = password;
+        }
+
+        public void addSession(DateTime startedDate)
+        {
+            Session s = new Session(startedDate);
+        }
+
+        public void addMeasurement(Measurement measurement)
+        {
+            Session s = Sessions.Last();
+            addMeasurement(s, measurement);
+        }
+
+
+        public void addMeasurement(Session session, Measurement measurement)
+        {
+            session.AddMeasurement(measurement);
+        }
+
+        public List<Session> getSessions()
+        {
+            return Sessions;
+        }
+
+        public Session getLastSession()
+        {
+            return Sessions.Last();
+        }
+       
     }
 }
