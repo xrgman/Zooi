758733ca1fd6b40ca7c5a324bc620aa71a058769
ren_keijzer <jcekeijze@avans.nl>
2015-09-25 11:52:48 +0200

    serverapplication namespace fixed

--- server application/Serverapplication.cs
+++ server application/Serverapplication.cs
@@ -7,11 +7,11 @@ using System.Net;
 using System.IO;
 using System.Threading;
 
-namespace WindowsFormsApplication1
+namespace server_application
 {
     class Serverapplication
     {
-        ArrayList<Measurement> data = new ArrayList<Measurement>();
+        List<Measurement> data = new List<Measurement>();
         public Serverapplication()
         {
             IPAddress ip = IPAddress.Parse("145.48.118.136");
@@ -35,8 +35,8 @@ namespace WindowsFormsApplication1
             while (!done)
             {
                 //data inlezen van client
-                    string received = ReadTextMessage(client);
-                    Console.WriteLine("recieved");
+                string received = ReadTextMessage(client);
+                Console.WriteLine("recieved");
                 ReadMessage(client);
             }
             client.Close();
