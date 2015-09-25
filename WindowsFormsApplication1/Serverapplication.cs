8f5c9fb5a6e7a45c336f224bb3803d8ecf0841b9
Marnix den Baars <m-denbaars1@hotmail.com>
2015-09-25 10:13:23 +0200

    serverapplication

--- WindowsFormsApplication1/Serverapplication.cs
+++ WindowsFormsApplication1/Serverapplication.cs
@@ -11,7 +11,7 @@ namespace WindowsFormsApplication1
 {
     class Serverapplication
     {
-
+        ArrayList<Measurement> data = new ArrayList<Measurement>();
         public Serverapplication()
         {
             IPAddress ip = IPAddress.Parse("145.48.118.136");
@@ -31,11 +31,13 @@ namespace WindowsFormsApplication1
         {
             TcpClient client = obj as TcpClient;
             bool done = false;
+            int totalReceived = 0;
             while (!done)
             {
                 //data inlezen van client
-                string received = ReadTextMessage(client);
-                Console.WriteLine("recieved");
+                    string received = ReadTextMessage(client);
+                    Console.WriteLine("recieved");
+                ReadMessage(client);
             }
             client.Close();
             Console.WriteLine("Connection closed");
@@ -56,6 +58,21 @@ namespace WindowsFormsApplication1
             stream.WriteLine(message);
             stream.Flush();
         }
+        public static string ReadMessage(TcpClient client)
+        {
 
+            byte[] buffer = new byte[256];
+            int totalRead = 0;
+
+            //read bytes until stream indicates there are no more
+            do
+            {
+                int read = client.GetStream().Read(buffer, totalRead, buffer.Length - totalRead);
+                totalRead += read;
+                Console.WriteLine("ReadMessage: " + read);
+            } while (client.GetStream().DataAvailable);
+
+            return Encoding.Unicode.GetString(buffer, 0, totalRead);
+        }
     }
 }
