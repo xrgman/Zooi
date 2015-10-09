e2b505ff6cb4c8ee21a44a7a93522d747f2b5683
kees2125 <kees2125@live.nl>
2015-10-09 08:27:23 +0200

    new packet addit

---
+++ ClassLibrary2/Packets/PacketRequestMeasurements.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Network.Packets
+{
+    [Serializable]
+    class PacketRequestMeasurements:Packet
+    {
+        string client;
+        DateTime date;
+
+        public PacketRequestMeasurements(string client,DateTime date)
+        {
+            this.client = client;
+            this.date = date;
+        }
+        
+    }
+}
