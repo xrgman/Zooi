f44b1f3ee565bef3d4d90d4592a4ac3dd905087f
DaveyMathijssen <daveym14@gmail.com>
2015-10-10 00:17:23 +0200

    video trial

---
+++ client aplication/WindowsFormsApplication1/Video.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Video : Form
+    {
+        public Video()
+        {
+            InitializeComponent();
+            axWindowsMediaPlayer1.URL = @"C:\Users\Davey\Videos\4K Video Downloader\15MinWorkout.mp4";
+        }
+
+    }
+}
